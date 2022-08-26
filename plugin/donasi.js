d96f01ce77714059fa2971d701e4285ce8f6e684
zeroostore01 <107806866+zeroostore01@users.noreply.github.com>
2022-08-26 19:22:24 +0700

    Update donasi.js

--- plugin/donasi.js
+++ plugin/donasi.js
@@ -13,7 +13,7 @@ Berapapun donasi kalian akan sangat berarti 👍
 𝐀𝐫𝐢𝐠𝐚𝐭𝐨𝐮!!!
 
 Contact person Owner:
-wa.me/6281511480762 (Owner)`
+wa.me/6283890585717 (Owner)`
 let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/ba6d17256c87499e83bb7.jpg' }}, { upload: conn.waUploadToServer })
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
      templateMessage: {
